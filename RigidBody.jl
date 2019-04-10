29b1c9bfe156bd1910b9182695fe1aa7d5fd911b
Vaishnav <vish0908@gmail.com>
2019-04-09 19:48:09 -0500

    Minor changes.

--- RigidBody.jl
+++ RigidBody.jl
@@ -6,10 +6,17 @@
 using LinearAlgebra
 include("OrientationConversion.jl");
 
+mutable struct extForces
+    Forces::Matrix{Float64}
+    Positions::Matrix{Float64}
+    Torques::Matrix{Float64}
+end
+
+
 mutable struct RigidBody
     m::Float64; # Mass of the rigid body.
     I::Matrix{Float64}; # Moment of inertia 3x3 matrix
-    orientation::String; # "321", "quarternions". Only these two supported now.
+    orientation::String; # "321", "quaternions". Only these two supported now.
     # This will determine how many states there are
     # 12 for euler angle, 13 for quartenions.
 
@@ -17,19 +24,21 @@ mutable struct RigidBody
     x::Vector{Float64};    # State vector
     dcm::Matrix{Float64};  # Inertial to body.
     invI::Matrix{Float64}; # inverse of moment of inertia, compute once during initialization.
-
+    J::Matrix{Float64} # 4x4 Inertia with first elemtn a random positive number
+    ω::Vector{Float64} # Angular velocity in body frame
     function RigidBody(mass::Float64, Inertia::Matrix{Float64}, orientation::String)
         if orientation == "321"
             x0 = Vector{Float64}(undef,12); # 12 state vector
-        elseif lowercase(orientation) == "quartenions"
-            x0 = Vector{Float64}(undef,13); # 13 state vector
+        elseif lowercase(orientation) == "quaternions"
+            x0 = Vector{Float64}(undef,14); # 14 state vector
         else
             error("Unknown orientation specified")
         end
 
         dcm = Matrix{Float64}(undef,3,3);
         invI = inv(Inertia);
-
+        J = [rand(1) zeros(1,3)
+             zeros(3,1) Inertia]
         xdot = function times2(in::Float64)::Float64
             return(2*in);
         end
@@ -37,47 +46,49 @@ mutable struct RigidBody
         this.m = mass;
         this.I = Inertia;
         this.orientation = lowercase(orientation);
+        this.dcm = dcm
         this.x = x0;
         this.invI = inv(Inertia);
+        this.J = J
+        this.ω = Vector{Float64}(undef,3)
         return this;
     end
 end
 
-function rbDynEuler(xdot::Vector{Float64}, RB::RigidBody,
-    PositionList::Matrix{Float64}, ForceList::Matrix{Float64},
-    TorqueList::Matrix{Float64}, GravityInInertial::Vector{Float64})
-    # States are: 12 states
-    # x = [x,y,z]:inertial, [u,v,w]:body, [ϕ,θ,ψ], [ω1,ω2,ω3]:body
-    # Forces in ForceList are decribed in body reference, ForceList is nFx3.
-    # PositionList are positions where forces are acting relative to cm,
-    # decribed in body reference, PositionList is nFx3.
-    # Torques in TorqueList are in body reference. TorqueList is matrix nTx3
+# function rbDynEuler(xdot::Vector{Float64}, RB::RigidBody,
+#     PositionList::Matrix{Float64}, ForceList::Matrix{Float64},
+#     TorqueList::Matrix{Float64}, GravityInInertial::Vector{Float64})
+#     # States are: 12 states
+#     # x = [x,y,z]:inertial, [u,v,w]:body, [ϕ,θ,ψ], [ω1,ω2,ω3]:body
+#     # Forces in ForceList are decribed in body reference, ForceList is nFx3.
+#     # PositionList are positions where forces are acting relative to cm,
+#     # decribed in body reference, PositionList is nFx3.
+#     # Torques in TorqueList are in body reference. TorqueList is matrix nTx3
+#
+#     xpos = RB.x[1];  # x position in inertial frame
+#     ypos = RB.x[2];  # y position in inertial frame
+#     h    = RB.x[3];  # z position in inertial frame
+#
+#     u = RB.x[4:6];   # Velocities in body reference frame
+#     ang = RB.x[7:9];     # Euler parameters
+#     ω = RB.x[10:12]; # Angular velocity  in body reference frame
+#
+#     # DCM w.r.t Euler parameters. vb = C*vn, inertial to body.
+#     ang2dcm(RB.dcm,ang,RB.orientation); # Have to code this up.
+#
+#     GravityInBody = RB.dcm*GravityInInertial;  # Convert to body reference
+#
+#     xdot[1:3] = RB.dcm'*u; # Velocities in inertial frame.
+#     xdot[4:6] = sum(ForceList,dims=1)/RB.m + GravityInBody; # Euler's first law.
+#     xdot[7:9] = # Code this
+#
+#     # ω dot equation -- Euler's second law
+#     TotalMoment = sum(cross(PositionList[i,:],ForceList[i,:]) for i in 1:size(Fi)[1]) + sum(TorqueList,dims=1);
+#     xdot[10:12] = RB.invI*(TotalMoment - cross(ω,RB.I*ω));
+# end
 
-    xpos = RB.x[1];  # x position in inertial frame
-    ypos = RB.x[2];  # y position in inertial frame
-    h    = RB.x[3];  # z position in inertial frame
-
-    u = RB.x[4:6];   # Velocities in body reference frame
-    ang = RB.x[7:9];     # Euler parameters
-    ω = RB.x[10:12]; # Angular velocity  in body reference frame
-
-    # DCM w.r.t Euler parameters. vb = C*vn, inertial to body.
-    ang2dcm(RB.dcm,ang,RB.orientation); # Have to code this up.
-
-    GravityInBody = RB.dcm*GravityInInertial;  # Convert to body reference
-
-    xdot[1:3] = RB.dcm'*u; # Velocities in inertial frame.
-    xdot[4:6] = sum(ForceList,dims=1)/RB.m + GravityInBody; # Euler's first law.
-    xdot[7:9] = # Code this
-
-    # ω dot equation -- Euler's second law
-    TotalMoment = sum(cross(PositionList[i,:],ForceList[i,:]) for i in 1:size(Fi)[1]) + sum(TorqueList,dims=1);
-    xdot[10:12] = RB.invI*(TotalMoment - cross(ω,RB.I*ω));
-end
-
-function rbDynQuat(xdot::Vector{Float64}, RB::RigidBody,
-    PositionList::Matrix{Float64}, ForceList::Matrix{Float64},
-    TorqueList::Matrix{Float64},GravityInInertial::Vector{Float64})
+function rbDynQuat!(xdot::Vector{Float64}, RB::RigidBody,
+    extF::extForces, Fc::Vector{Float64}, GravityInInertial::Vector{Float64})
 
     # States are: 14 states
     # x = [x,y,z]:inertial, [u,v,w]:inertial, [β0,β1,β2,β3], [β̇0,β̇1,β̇2,β̇3]
@@ -91,15 +102,18 @@ function rbDynQuat(xdot::Vector{Float64}, RB::RigidBody,
     ypos = RB.x[2];  # y position in inertial frame
     h    = RB.x[3];  # z position in inertial frame
 
-    u = RB.x[4:6];   # Velocities in Inertial reference frame
-    β = RB.x[7:10];  # Euler parameters
+    u = RB.x[8:10];   # Velocities in Inertial reference frame
+    β = RB.x[4:7];  # Euler parameters
     βdot = RB.x[11:14] # Euler parameter derivatives
-    ω = angVel(β,βdot)
+    angVel(RB.ω,β,βdot)
     β0 = β[1];
     β1 = β[2];
     β2 = β[3];
     β3 = β[4];
 
+    ForceList = extF.Forces
+    PositionList = extF.Positions
+    TorqueList = extF.Torques
 
     # DCM w.r.t Euler parameters. vb = C*vn, inertial to body.
     quat2dcm(RB.dcm,β);
@@ -116,7 +130,8 @@ function rbDynQuat(xdot::Vector{Float64}, RB::RigidBody,
     #     ];
     # xdot[7:10] = Ω*β; # Not accurate -- need Udwadia's formulation.
     # ω dot equation -- Euler's second law
-    TotalMoment = sum(cross(PositionList[i,:],ForceList[i,:]) for i in 1:size(Fi)[1]) + sum(TorqueList,dims=1);
+    TotalForce = sum(ForceList,dims=1) + m*GravityInInertial + Fc[1:3]
+    TotalMoment = sum(cross(PositionList[i,:],ForceList[i,:]) for i in 1:size(ForcesList)[1]) + sum(TorqueList,dims=1) + Fc[4:end]
     # xdot[11:13] = RB.invI*(TotalMoment - cross(ω,RB.I*ω));
 
     # Udwadia's formulation using 14 States
@@ -124,27 +139,7 @@ function rbDynQuat(xdot::Vector{Float64}, RB::RigidBody,
           -β2 -β3  β0  β1
           -β3  β2 -β1  β0]
     xdot[1:3] = u
-    xdot[4:6] = transpose(RB.dcm)*sum(ForceList,dims=1)/RB.m + GravityInInertial
-    xdot[7:10] = βdot
-    xdot[11:14] = -0.5*transpose(E1)*RB.invI*skewX(ω)*RB.I*ω - (transpose(βdot)*βdot)*β + transpose(E1)*RB.invI*E1*TotalMoment/4
-
-
-end
-
-function skewX(X::Matrix{Float64},x::Vector{Float64})
-    X = [    0 -x[3]  x[2]
-          x[3]     0 -x[1]
-         -x[2]  x[1]    0]
-end
-
-function angVel(ω::Vector{Float64},β::Vector{Float64},βdot::Vector{Float64})
-    β0 = β[1];
-    β1 = β[2];
-    β2 = β[3];
-    β3 = β[4];
-
-    E1 = [-β1  β0  β3 -β2
-          -β2 -β3  β0  β1
-          -β3  β2 -β1  β0]
-    ω = E1*βdot
+    xdot[8:10] = transpose(RB.dcm)*sum(ForceList,dims=1)/RB.m + GravityInInertial
+    xdot[4:7] = βdot
+    xdot[11:14] = -0.5*transpose(E1)*RB.invI*skewX(RB.ω)*RB.I*RB.ω - (transpose(βdot)*βdot)*β + transpose(E1)*RB.invI*E1*TotalMoment/4
 end
--- test.jl
+++ test.jl
@@ -1,22 +1,31 @@
-include("RigidBody.jl");
-I1 = [1 0 0; 0 0.1 0; 0 0 1.2];
-m = 1.0;
-
-R = RigidBody(m,I1,"321");
-
-include("OrientationConversion.jl");
-
+include("simulate.jl")
+clearconsole()
+I1 = [1 0 0; 0 0.1 0; 0 0 1.2]
+m = 1.0
+using Revise
 # Test the code.
-for i in 1:100
-    #ang0 = [45*pi/180;60*pi/180;30*pi/180];
-    ang0 = rand(3);
-    q = angle2quat(ang0,"321");
-    ang = quat2angle(q,"321");
-    e = ang - ang0;
-    println("Norm error:", norm(e));
-end
+# for i in 1:100
+#     #ang0 = [45*pi/180;60*pi/180;30*pi/180];
+#     ang0 = rand(3);
+#     q = angle2quat(ang0,"321");
+#     ang = quat2angle(q,"321");
+#     e = ang - ang0;
+#     println("Norm error:", norm(e));
+# end
 
 # Simulate rigid body dynamics
 # We have to now call ode solver with parameters
 # -- this will have a function that will be called to compute all time varying
 # -- quantities.
+
+
+# Testing Dynamics with Revolute Joint
+R1 = RigidBody(m,I1,"quaternions")
+R2 = RigidBody(m,I1,"quaternions")
+axis = [0.0 0 1][:]
+rj1 = [1 0 0.0][:]
+rj2 = [1 0 0.0][:]
+j = RevJoint(R1,R2,rj1,rj2,axis)
+tEnd = 1.0
+tSpan = 0.01
+simulate(tEnd,tSpan,j)
