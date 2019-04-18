e04ebc5e025e1beb77ff3f7646dbdd2cba56060b
Vaishnav <vish0908@gmail.com>
2019-04-18 12:32:21 -0500

    Using inplace convention. Added Plots call.

--- simplePendulumMinReal.jl
+++ simplePendulumMinReal.jl
@@ -2,27 +2,29 @@ using LinearAlgebra
 using DifferentialEquations
 using Revise
 using JLD
+using Plots; pyplot()
+include("plotSol.jl")
 clearconsole()
 # cd("/home/vish0908/Documents/ModRob/ISRL-MBD")
 
 global m = 1.0
 global l = 1.0
-function thdot(th::Vector{Float64})
+function thdot!(thdot::Vector{Float64},th::Vector{Float64})
     global l, m
-    thdot = zeros(2)
+    # thdot = zeros(2)
     thdot[1] = th[2]
     thdot[2]  = -3*9.806/2/l*sin(th[1])
-    return thdot
+    # return thdot
 end
 
-function mainDynMinReal(x,p,t)
-    dx = thdot(x)
-    return dx
+function mainDynMinReal!(dx,x,p,t)
+    thdot!(dx,x)
+    # return dx
 end
 
 tEnd = 1.0; tSpan = 0.01;
 x0 = [pi/2;0.0]
-prob = ODEProblem(mainDynMinReal,x0,(0.0,tEnd))
+prob = ODEProblem(mainDynMinReal!,x0,(0.0,tEnd))
 sol = solve(prob,Tsit5(),saveat = tSpan,reltol=1e-10,abstol=1e-10)
 tSim = sol.t
 thSol = sol[1,:]
@@ -38,7 +40,8 @@ for i=1:length(tSim)
 end
 rSol = [xSol ySol zSol]
 ωSol = [ySol thDotSol ySol]
-save("PendulumMin.jld","rMin",rSol,"ωMin", ωSol,"vMin",vSol)
+plotPos(tSim,rSol)
+# save("PendulumMin.jld","rMin",rSol,"ωMin", ωSol,"vMin",vSol)
 
 # rSol = transpose(sol[1:3,:])
 # vSol = transpose(sol[4:6,:])
