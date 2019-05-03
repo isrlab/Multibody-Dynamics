212dcd22390ba687df4f089861f84751ca42a296
Vaishnav <vish0908@gmail.com>
2019-05-03 16:04:01 -0500

    extForces.

--- simulate.jl
+++ simulate.jl
@@ -52,20 +52,20 @@ function mainDynODE(X::Vector{Float64},j::Tuple{Vararg{Joint}},t::Float64)
     # Create extForcesList storing extForces for each rigid body
     # Create ForceConstraints Array storing constraint forces acting on each rigid body
     global GravityInInertial
-
     # Update RigidBodies
     updateRigidBody(j[1].RB1,X[1:14])
     for k=1:length(j)
         updateRigidBody(j[k].RB2,X[14*k+1:14*(k+1)])
     end
-
+    @show t
     # Generate extForces for each body
     extFList = Vector{extForces}(undef,length(j)+1)
     extFList[1] = getExternalForce(j[1].RB1)
     for k=2:length(j)+1
         extFList[k] = getExternalForce(j[k-1].RB2)
     end
-
+    # extFList[2] = extForces(transpose((j[1].RB2.m)*-GravityInInertial),zeros(1,3),[10.0 0.0 0.0])
+    extFList[3] = extForces(transpose((j[1].RB2.m+j[2].RB2.m)*-GravityInInertial),zeros(1,3),[0.0 0.0 10.0])
     # Generate constraint Forces for each body using UK Formulation
     ForceConstr = Array{Float64}(undef,7,length(j)+1)
     ForceConstr[:,2] = ForceCon(j[1],extFList[1],extFList[2],GravityInInertial)
@@ -75,7 +75,6 @@ function mainDynODE(X::Vector{Float64},j::Tuple{Vararg{Joint}},t::Float64)
             ForceConstr[:,k+1] = ForceCon(j[k],extFList[k],extFList[k+1],GravityInInertial)[8:14]
         end
     end
-
 
     dX = mainDyn(X,j,extFList,ForceConstr, GravityInInertial)
     return dX
