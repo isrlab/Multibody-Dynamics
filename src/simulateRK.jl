8d876a5c24bd5e99dcc7b4b845e5872df1355e42
Vaishnav <vish0908@gmail.com>
2019-05-30 15:32:17 -0500

    Changed name.

--- src/simulateRK.jl
+++ src/simulateRK.jl
@@ -8,6 +8,7 @@ include("Joint.jl")
 include("Force.jl")
 include("extF.jl")
 include("rk45.jl")
+include("rk4.jl")
 
 using LinearAlgebra
 using Revise
@@ -25,6 +26,7 @@ struct solSim
     βdot::Matrix{Float64}
 end
 
+## RK45
 function simulateRK45(tEnd::Float64,tSpan::Float64,j::Joint...;
                   g::MArray{Tuple{3},Float64,1,3}=[0.0;0.0;-9.806])#,extFVec::Vector{extForces}=Vector{extForces}(undef,1))
     # Ellipsis (...) to facilitate supplying variable number of arguments to the function
@@ -36,22 +38,66 @@ function simulateRK45(tEnd::Float64,tSpan::Float64,j::Joint...;
         append!(X0,j[k].RB2.x)
     end
 
-    # Using native RK45 implementation
-    solFinal = rk45(tEnd,0.0,X0,j,mainDynRK45,eps = 1e-10)
-
-
-    # solFinal = Vector{solSim}(undef,length(j))
-    # for i=1:length(j)
-    #     rSol = transpose(sol[14*i+1:14*i+3,:])
-    #     vSol = transpose(sol[14*i+8:14*i+10,:])
-    #     βSol = transpose(sol[14*i+4:14*i+7,:])
-    #     βdotSol = transpose(sol[14*i+11:14*i+14,:])
-    #     solFinal[i] = solSim(rSol,vSol,βSol,βdotSol)
-    # end
-    return solFinal
+    # Using native RK implementation
+    tSim, sol = rk45(tEnd,0.0,X0,j,mainDynRK,eps = 1e-6)
+    nBodies = length(j) + 1
+    ntStops = Int64(length(sol)/(14*nBodies))
+    sol = reshape(sol,(14*nBodies,ntStops))
+    @show sol
+    error("break.")
+    solFinal = Vector{solSim}(undef,length(j))
+    rSol = Matrix{Float64}(undef,ntStops,3)
+    vSol = Matrix{Float64}(undef,ntStops,3)
+    βSol = Matrix{Float64}(undef,ntStops,4)
+    βdotSol = Matrix{Float64}(undef,ntStops,4)
+    for i=1:length(j)
+        for t=1: ntStops
+            rSol[t,:] = sol[14*i+1:14*i+3,t]
+            vSol[t,:] = sol[14*i+8:14*i+10,t]
+            βSol[t,:] = sol[14*i+4:14*i+7,t]
+            βdotSol[t,:] = sol[14*i+11:14*i+14,t]
+        end
+        solFinal[i] = solSim(rSol,vSol,βSol,βdotSol)
+    end
+    return tSim, solFinal
 end
 
-function mainDynRK45(t::Float64, x::Vector{Float64}, p::Tuple{Vararg{Joint}})
+## RK4
+function simulateRK4(tEnd::Float64,tSpan::Float64,j::Joint...;
+                  g::MArray{Tuple{3},Float64,1,3}=[0.0;0.0;-9.806])#,extFVec::Vector{extForces}=Vector{extForces}(undef,1))
+    # Ellipsis (...) to facilitate supplying variable number of arguments to the function
+    # Initial condition (all bodies connected)
+    global GravityInInertial = g
+    # global extFList = extFVec
+    X0 = j[1].RB1.x
+    for k = 1:length(j)
+        append!(X0,j[k].RB2.x)
+    end
+
+    # Using native RK implementation
+    sol = rk4(tEnd,0.0,X0,j,mainDynRK, h = tSpan)
+    nBodies = length(j) + 1
+    ntStops = Int64(length(sol)/(14*nBodies))
+    tSim, sol = reshape(sol,(14*nBodies,ntStops))
+
+    solFinal = Vector{solSim}(undef,length(j))
+    rSol = Matrix{Float64}(undef,ntStops,3)
+    vSol = Matrix{Float64}(undef,ntStops,3)
+    βSol = Matrix{Float64}(undef,ntStops,3)
+    βdotSol = Matrix{Float64}(undef,ntStops,3)
+    for t=1: ntStops
+        for i=1:length(j)
+            rSol[t,:] = sol[14*i+1:14*i+3,t]
+            vSol[t,:] = sol[14*i+8:14*i+10,t]
+            βSol[t,:] = sol[14*i+4:14*i+7,t]
+            βdotSol[t,:] = sol[14*i+11:14*i+14,t]
+        end
+        solFinal[i] = solSim(rSol,vSol,βSol,βdotSol)
+    end
+    return tSim, solFinal
+end
+
+function mainDynRK(t::Float64, x::Vector{Float64}, p::Tuple{Vararg{Joint}})
     dx = Vector{Float64}(undef,length(x))
     return mainDynODE!(dx,x,p,t)
 end
@@ -66,7 +112,6 @@ function mainDynODE!(dX::Vector{Float64}, X::Vector{Float64}, j::Tuple{Vararg{Jo
     for k=1:length(j)
         updateRigidBody!(j[k].RB2,X[14*k+1:14*(k+1)])
     end
-    @show t
 
     extFListCopy = externalFTotal(t,j) # Generates all the external forces explicitly specified, through joint actions or directly.
 
